8e161fb032db637ce3a633d04a0091b8d91084db
Nayara <nayaramaisa.stn@gmail.com>
2025-05-19 13:21:21 -0300

    finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,55 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem{
+  id:number;
+  task:string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor,NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'ToDoListAngular';
+  todoList :TodoItem[]= [];
+  newTask : string='';
+
+  ngOnInit(){
+    const saved = localStorage.getItem('todoList');
+    if(saved){
+      this.todoList= JSON.parse(saved);
+    }
+  }
+
+  addTask():void{
+    if(this.newTask.trim()!== ''){
+      const newTodoItem: TodoItem ={
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+      this.todoList.push(newTodoItem)
+      this.newTask=''
+    }
+    localStorage.setItem('todoList',JSON.stringify(this.todoList));
+  }
+  toggleCompleted(index:number):void{
+    this.todoList[index].completed = !this.todoList[index].completed
+    localStorage.setItem('todoList',JSON.stringify(this.todoList));
+  }
+
+  deleteTask(id:number): void{
+    this.todoList= this.todoList.filter(item => item.id !== id)
+    console.log(this.todoList)
+    localStorage.setItem('todoList',JSON.stringify(this.todoList));
+  }
 }
+
+
+
